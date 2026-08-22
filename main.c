00c4256c8fcb68fff506e36dca3e169e9309943f
Apologizes <apolo@apologizes.org>
2026-08-22 19:54:26 -0400

    feat(main): implement printing postfix in readable format and a debug option

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-Token* lexical_analysis(const char *c) {
-    printf("===== Lexical Analysis =====\n\n");
+#define unlikely(a) __builtin_expect(!!(a), 0)
 
-    printf("Input Script:\n");
-    printf("%s\n\n", c);
-    printf("Lexical Analysis Output:\n\n");
-
+Token* lexical_analysis(const char *c, bool dump_tokens) {
+    if (unlikely(dump_tokens == true)) { printf("===== Lexical Analysis =====\n\n"); }
+    printf("\nInfix Script:\n");
+    printf("%s\n", c);
+    if (unlikely(dump_tokens == true)) {  printf("Lexical Analysis Output:\n\n"); }
     lexer_init(c);
 
     Token count_tokens;
@@ -32,15 +33,30 @@ Token* lexical_analysis(const char *c) {
         t = lexer_next_token();
         output[current_token++] = t;
 
-        printf("Type: %2d, Column: %2d, Line: %2d, Value: %g\n", t.type, t.column, t.line, t.value);
+        if (unlikely(dump_tokens == true)) { printf("Type: %2d, Column: %2d, Line: %2d, Value: %g\n", t.type, t.column, t.line, t.value); }
     } while (t.type != TOKEN_EOF && current_token < token_count);
-    printf("\nTotal Tokens: %2ld\n", total_tokens);
-
-    if (t.type == TOKEN_UNKOWN) {
-        printf("Unkown token");
-    }
-    printf("\n===== End of Lexical Analysis =====\n\n");
+    if (unlikely(dump_tokens == true)) {
+        printf("\nTotal Tokens: %2ld\n", total_tokens);
+        printf("\n===== End of Lexical Analysis =====\n\n"); }
     return output;
+}
+
+static inline void print_actual_token(Token t) {
+    char *space = " ";
+    switch (t.type) {
+        case TOKEN_NUM_LIT: printf("%g%s",t.value,  space); break;
+        case TOKEN_ROOT: printf("./%s", space); break;
+        case TOKEN_EXP: printf("^%s", space); break;
+        case TOKEN_PLUS: case TOKEN_ADD: printf("+%s", space); break;
+        case TOKEN_POS: printf("+(pos)%s", space); break;
+        case TOKEN_MINUS: case TOKEN_SUB: printf("-%s", space); break;
+        case TOKEN_NEG: printf("-(neg)%s", space); break;
+        case TOKEN_IMPLICIT_MUL: case TOKEN_MUL: printf("*%s", space); break;
+        case TOKEN_DIV: printf("/%s", space); break;
+        case TOKEN_REM: printf("%%%s", space); break;
+        case TOKEN_EOF: printf("EOF"); return;
+        default: fprintf(stderr, "How?"); exit(EXIT_FAILURE);
+    }
 }
 
 static char *read_file_contents(const char *path) {
@@ -85,33 +101,51 @@ static inline void print_tokens(Token *t, int64_t tokens) {
 int main(const int argc, char **argv) {
     if (argc < 2) {
         printf("Please provide a file");
-        return 0;
+        return 1;
     }
 
+    bool dump_tokens = false;
+
     const char *source_path = argv[1];
+    for (int64_t i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "-d") == 0) {
+            dump_tokens = true;
+        }
+    }
+
     const char *source_code = read_file_contents(source_path);
-    Token *t = lexical_analysis(source_code);
+    Token *t = lexical_analysis(source_code, dump_tokens);
 
     int64_t imp_count;
-    printf("===== Imp Mul Resolvement =====\n\n");
+    if (unlikely(dump_tokens == true)) { printf("===== Imp Mul Resolvement =====\n\n"); }
     Token *imp = resolve_imp_mul(t, total_tokens, &imp_count);
-    //free(t);
-    print_tokens(imp, imp_count);
-    printf("===== End of Imp Mul Resolvement =====\n\n");
+    if (unlikely(dump_tokens == true)) {
+        print_tokens(imp, imp_count);
+        printf("===== End of Imp Mul Resolvement =====\n\n");
+    }
     resolve_arity(imp, imp_count);
-    printf("===== Arity Resolvement =====\n\n");
-    print_tokens(imp, -1);
-    printf("===== End of Arity Resolvement =====\n\n");
+    if (unlikely(dump_tokens == true)) {
+        printf("===== Arity Resolvement =====\n\n");
+        print_tokens(imp, -1);
+        printf("===== End of Arity Resolvement =====\n\n");
+    }
     int64_t out_count;
     Token *postfix = shunting_yard(imp, imp_count, &out_count);
     free(imp);
-    printf("===== Postfix Conversion =====\n\n");
-    print_tokens(postfix, out_count);
-    printf("===== End of Postfix Conversion =====\n\n");
+    if (unlikely(dump_tokens == true)) {
+        printf("===== Postfix Conversion =====\n\n");
+        print_tokens(postfix, out_count);
+        printf("===== End of Postfix Conversion =====\n\n");
+    }
     double result = evaluate_postfix_throwaway(postfix, t, out_count);
+    printf("\nPostfix:\n");
+    int64_t current_token = 0;
+    do {
+        print_actual_token(postfix[current_token++]);
+    } while (postfix[current_token].type != TOKEN_EOF && current_token < out_count);
     free(postfix);
 
-    printf("Result: %g\n\n", result);
+    printf("\n\nResult: %g\n\n", result);
 
     return 0;
 }
